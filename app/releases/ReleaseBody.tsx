dc6fb1cfdfe51be92fec1575a9fedb1c726fcfc3
fallow64 <fallow640@gmail.com>
2025-03-11 15:57:43 -0500

    Make sure commit is not already part of link

--- app/releases/ReleaseBody.tsx
+++ app/releases/ReleaseBody.tsx
@@ -2,9 +2,21 @@ import MarkdownContainer from '@/components/MarkdownContainer';
 
 const ReleaseBody = ({ content }: { content: string }) => {
   // Format 40-character hex strings as links to GitHub commits
-  const formatted = content.replace(/([0-9A-Fa-f]{40})/g, (fullCommit) => {
-    return `[${fullCommit.substring(0, 8)}](https://github.com/PelicanPlatform/pelican/commit/${fullCommit})`;
-  });
+  const formatted = content.replace(
+    /([0-9A-Fa-f]{40})/g,
+    (match, _hash, offset, fullText) => {
+      // Ensure the hash is not part of a markdown link like (text)[url]
+      const afterIndex = offset + match.length;
+      const after = fullText.substring(afterIndex, afterIndex + 1);
+
+      if (after === ')' || after === ']') {
+        // do not modify
+        return match;
+      } else {
+        return `[${match.substring(0, 8)}](https://github.com/PelicanPlatform/pelican/commit/${match})`;
+      }
+    }
+  );
   console.log(formatted);
   return <MarkdownContainer content={formatted} />;
 };
